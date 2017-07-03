99d6a9c56a063f74287de60ab25845dc087ea6af
Sam Tay <sam.chong.tay@gmail.com>
2017-07-03 00:21:21 -0400

    Keep track of highest score and congratulate player

--- app/Main.hs
+++ app/Main.hs
@@ -4,20 +4,25 @@ import Control.Monad (when)
 import Data.Maybe (fromMaybe)
 import Data.Monoid ((<>))
 import System.Exit (exitSuccess)
+import System.IO (readFile, writeFile)
+import Text.Read (readMaybe)
 
 import Tetris (Game(..))
 import UI.PickLevel (pickLevel)
 import UI.Game (playGame)
 
 import Options.Applicative
+import qualified System.Directory as D
+import System.FilePath ((</>))
+import qualified System.FilePath as F
 
 data Opts = Opts
   { hardDrop :: Maybe HardDropOpt
   , level    :: Maybe Int
   , score    :: Bool
-  } deriving (Show) -- TODO remove
+  }
 
-data HardDropOpt = AsciiOnly | CustomChars String deriving (Show) -- TODO remove
+data HardDropOpt = AsciiOnly | CustomChars String
 
 opts :: Parser Opts
 opts = Opts
@@ -30,7 +35,6 @@ opts = Opts
   <*> switch
     (  long "high-score"
     <> help "Print high score and exit" )
-
 
 hardDropOpt :: Parser HardDropOpt
 hardDropOpt = asciiOpt <|> custOpt
@@ -63,15 +67,44 @@ hdOptStr (CustomChars s) = s
 
 main :: IO ()
 main = do
-  (Opts hd ml hs) <- execParser fullopts          -- get CLI opts/args
-  let mp = hdOptStr <$> hd                        -- determine hard drop preview cell
-  when hs (getHighScore >>= print >> exitSuccess) -- show high score and exit
-  l <- fromMaybe pickLevel (return <$> ml)        -- pick level prompt if necessary
-  g <- playGame l mp                              -- play game
-  handleEndGame (_score g)                        -- save & print score
+  (Opts hd ml hs) <- execParser fullopts           -- get CLI opts/args
+  let mp = hdOptStr <$> hd                         -- determine hard drop preview cell
+  when hs (getHighScore >>= printM >> exitSuccess) -- show high score and exit
+  l <- fromMaybe pickLevel (return <$> ml)         -- pick level prompt if necessary
+  g <- playGame l mp                               -- play game
+  handleEndGame (_score g)                         -- save & print score
 
 handleEndGame :: Int -> IO ()
-handleEndGame = const $ return ()
+handleEndGame s = do
+  mhs <- getHighScore
+  case mhs of
+    Nothing -> newHighScore
+    Just hs -> if s <= hs then justShowScore else newHighScore
+  where
+    justShowScore = putStrLn $ "Your final score: " ++ show s
+    newHighScore = do
+      putStrLn $ "Congrats! You just got the new highest score: " ++ show s
+      setHighScore s
 
-getHighScore :: IO Int
-getHighScore = error "Not yet implemented"
+printM :: Show a => Maybe a -> IO ()
+printM Nothing  = putStrLn "None"
+printM (Just s) = print s
+
+getHighScore :: IO (Maybe Int)
+getHighScore = do
+  lb <- getLeaderboardFile
+  exists <- D.doesFileExist lb
+  if exists
+     then readMaybe <$> readFile lb
+     else return Nothing
+
+setHighScore :: Int -> IO ()
+setHighScore s = do
+  lb <- getLeaderboardFile
+  writeFile lb (show s)
+
+getLeaderboardFile :: IO FilePath
+getLeaderboardFile = do
+  xdg <- D.getXdgDirectory D.XdgData "tetris"
+  D.createDirectoryIfMissing True xdg
+  return (xdg </> "leaderboard")
