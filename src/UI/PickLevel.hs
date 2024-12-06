a3ca6ecdd268c1147326246ad197cf2de767b3d5
aaditagrawal <103925638+aaditagrawal@users.noreply.github.com>
2024-12-06 15:57:31 +0530

    feat: Add level progression option to level picker
    - Introduce LevelConfig type to bundle level number and progression setting
    - Add two-step menu system for selecting level and progression mode
    - Implement UP/DOWN navigation and selection for progression options
    - Add visual highlighting for selected menu options
    - Update UI to show different screens for level and progression selection

--- src/UI/PickLevel.hs
+++ src/UI/PickLevel.hs
@@ -1,5 +1,6 @@
 module UI.PickLevel
   ( pickLevel
+  , LevelConfig(..)
   ) where
 
 import System.Exit (exitSuccess)
@@ -11,17 +12,39 @@ import qualified Brick.Widgets.Border.Style as BS
 import qualified Brick.Widgets.Center as C
 import qualified Graphics.Vty as V
 
-app :: App (Maybe Int) e ()
+data LevelConfig = LevelConfig
+  { levelNumber :: Int
+  , progression :: Bool
+  } deriving (Show, Eq)
+
+data MenuOption = YesOption | NoOption deriving (Eq)
+
+data PickState = PickState
+  { currentLevel :: Maybe Int
+  , showProgression :: Bool
+  , pickingLevel :: Bool
+  , selectedOption :: MenuOption
+  }
+
+app :: App PickState e ()
 app = App
-  { appDraw         = const [ui]
+  { appDraw         = drawUI
   , appHandleEvent  = handleEvent
   , appStartEvent   = pure ()
-  , appAttrMap      = const $ attrMap V.defAttr []
+  , appAttrMap      = const $ attrMap V.defAttr
+      [ (selectedAttr, V.black `on` V.white)
+      ]
   , appChooseCursor = neverShowCursor
   }
 
-ui :: Widget ()
-ui =
+selectedAttr :: AttrName
+selectedAttr = attrName "selected"
+
+drawUI :: PickState -> [Widget ()]
+drawUI ps = [ui ps]
+
+ui :: PickState -> Widget ()
+ui ps =
   padLeft (Pad 19)
     $ padRight (Pad 21)
     $ C.center
@@ -30,17 +53,62 @@ ui =
     $ withBorderStyle BS.unicodeBold
     $ B.borderWithLabel (str "Tetris")
     $ C.center
-    $ str " Choose Level (0-9)"
+    $ vBox
+    [ if pickingLevel ps
+        then str "Choose Level (0-9)"
+        else vBox
+          [ str "Level Progression?"
+          , str ""
+          , drawOption "YES" YesOption (selectedOption ps)
+          , drawOption "NO" NoOption (selectedOption ps)
+          , str ""
+          , str "Use ↑↓ to select"
+          , str "Press Enter to continue"
+          ]
+    ]
 
-handleEvent :: BrickEvent () e -> EventM () (Maybe Int) ()
+drawOption :: String -> MenuOption -> MenuOption -> Widget ()
+drawOption label opt current =
+  withAttr (if opt == current then selectedAttr else attrName "")
+    $ str $ "  " ++ label ++ "  "
+
+handleEvent :: BrickEvent () e -> EventM () PickState ()
 handleEvent (VtyEvent (V.EvKey V.KEsc        _)) = halt
 handleEvent (VtyEvent (V.EvKey (V.KChar 'q') _)) = halt
 handleEvent (VtyEvent (V.EvKey (V.KChar 'Q') _)) = halt
 handleEvent (VtyEvent (V.EvKey (V.KChar d) [])) =
-  when (d `elem` ['0' .. '9']) $ do
-    put $ Just $ read [d]
-    halt
+  whenPickingLevel $ when (d `elem` ['0' .. '9']) $ do
+    modify $ \s -> s { currentLevel = Just $ read [d], pickingLevel = False }
+handleEvent (VtyEvent (V.EvKey V.KEnter [])) = do
+  s <- get
+  when (not $ pickingLevel s) $ do
+    case currentLevel s of
+      Just l -> do
+        put $ PickState (Just l) (selectedOption s == YesOption) True YesOption
+        halt
+      Nothing -> pure ()
+handleEvent (VtyEvent (V.EvKey V.KUp [])) =
+  whenNotPickingLevel $ modify $ \s -> s { selectedOption = YesOption }
+handleEvent (VtyEvent (V.EvKey V.KDown [])) =
+  whenNotPickingLevel $ modify $ \s -> s { selectedOption = NoOption }
 handleEvent _ = pure ()
 
-pickLevel :: IO Int
-pickLevel = defaultMain app Nothing >>= maybe exitSuccess return
+whenPickingLevel :: EventM () PickState () -> EventM () PickState ()
+whenPickingLevel action = do
+  picking <- gets pickingLevel
+  when picking action
+
+whenNotPickingLevel :: EventM () PickState () -> EventM () PickState ()
+whenNotPickingLevel action = do
+  picking <- gets pickingLevel
+  when (not picking) action
+
+initialState :: PickState
+initialState = PickState Nothing True True YesOption
+
+pickLevel :: IO LevelConfig
+pickLevel = do
+  result <- defaultMain app initialState
+  case currentLevel result of
+    Nothing -> exitSuccess
+    Just l -> return $ LevelConfig l (showProgression result)
